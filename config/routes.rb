8a31943af14dc579298608cb4f64fc89b31ab551
HUILI <root33hash@gmail.com>
2018-02-22 00:23:58 +0900

    chapter7.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
 
   # get 'static_pages/home'
   # get 'static_pages/help'
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
