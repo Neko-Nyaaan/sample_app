2c834f3077b6c0142d90a5f70caa77c4875c1106
HUILI <root33hash@gmail.com>
2018-02-23 02:27:44 +0900

    wip feature/chapter8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      login_url user
+      redirect_to user
       #ユーザーログイン後にユーザー情報のページにリダイレクト
     else
       #エラーメッセージを作成する
@@ -12,5 +14,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
